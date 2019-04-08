0b6d7e6b34bf0f0d0344c07f3fd2a380468711b0
Jefferson Tora <hi@jeffersontora.com>
2019-04-08 09:57:37 -0300

    Change style of movie item

--- src/components/MovieItem/styles.js
+++ src/components/MovieItem/styles.js
@@ -2,48 +2,52 @@ import styled from 'styled-components';
 import { colors } from '../../styles';
 
 export const MovieCard = styled.div`
-  position:relative;
-  background:${colors.white};
-  transition: all .2s ease-in-out;
+  position: relative;
+  background: ${colors.white};
+  transition: all 0.2s ease-in-out;
 
   &:hover {
-    transform:scale(1.05);
+    transform: scale(1.05);
   }
 
   h2 {
-    text-align:center;
-    font-size:1em;
-    font-weight:400;
-    color:${colors.regular}
+    text-align: center;
+    font-size: 1em;
+    font-weight: 400;
+    color: ${colors.regular};
   }
 
   a {
-    text-decoration:none;
+    text-decoration: none;
   }
 
   span {
-    position:absolute;
-    top:12px;
-    left:10px;
-    font-weight:600;
+    position: absolute;
+    top: 12px;
+    left: 10px;
+    font-weight: 600;
     color: ${colors.white};
+    z-index: 99;
   }
-
 `;
 
 export const ImageContainer = styled.div`
-  position:relative;
-  min-height:340px;
+  position: relative;
+  min-height: 340px;
   overflow: hidden;
 
-  &:before{
-    content:'';
-    position:absolute;
-    width:100%;
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
     height: 50%;
-    pointer-events:none;
-    z-index:99;
-    background: linear-gradient(to bottom, ${colors.darkTransparent} 0%, ${colors.transparent}  100% ) ;
+    pointer-events: none;
+    z-index: 99;
+    background: linear-gradient(
+      to bottom,
+      ${colors.darkTransparent} 0%,
+      ${colors.transparent} 100%
+    );
   }
 
   img {
@@ -53,8 +57,6 @@ export const ImageContainer = styled.div`
     height: auto;
     top: 50%;
     left: 50%;
-    transform: translate( -50%, -50%);
+    transform: translate(-50%, -50%);
   }
 `;
-
-
